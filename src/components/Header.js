0952b09a207d83515a0ab91f11c709c21b86918b
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-07 23:42:54 +0700

    Conditional render components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,11 @@
 import React from "react";
 import logo from "../logo.svg";
-import { Content } from "./Content";
 
 const Header = (props) => {
-  const multiply = (a, b) => a * b;
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1>{props.title}</h1>
-      <div>{props.num}</div>
-      <div>
-        {props.myArr[0]}, {props.myArr[1]}, {props.myArr[2]}
-      </div>
-      <Content
-        myObj={props.myObj}
-        myFunc={props.myFunc}
-        myLambdaFunc={props.myLambdaFunc}
-      />
-      <Content
-        myObj={{ name: "Delima", age: 18 }}
-        myFunc={multiply}
-        myLambdaFunc={(a, b) => a / b}
-      />
     </header>
   );
 };
