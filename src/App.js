82d4abc9ca9fafe257278c94f8c83d35e5357640
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-07 20:44:08 +0700

    Create components..

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-// function component
-const Content = () => {
-  return (
-    <div>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </div>
-  );
-};
+// class component
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
 
 // function component
-function Header() {
+const Header = () => {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <Content />
     </header>
   );
-}
+};
 
-// class component
 class App extends Component {
   render() {
     return (
