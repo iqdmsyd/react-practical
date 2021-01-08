394197931203046ba7a75d6bed0a5a681bc12db0
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-08 15:36:43 +0700

    lifting up state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
-import FetchRandomUser from "./components/FetchRandomUser";
+import Counter from "./components/Counter";
 
 class App extends Component {
   state = {
-    visible: true,
+    count: 0,
+  };
+
+  componentDidMount() {
+    console.log("mounting...");
+  }
+
+  componentWillUnmount() {
+    console.log("unmounting...");
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
   };
 
   render() {
     return (
       <div className="App">
-        <FetchRandomUser />
+        <Counter
+          count={this.state.count}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
+        <Counter
+          count={this.state.count}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
       </div>
     );
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,12 @@
 import { Component } from "react";
 
 export default class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  componentDidMount() {
-    console.log("mounting...");
-  }
-
-  componentWillUnmount() {
-    console.log("unmounting...");
-  }
-
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
   render() {
     return (
       <div>
-        <div>counter: {this.state.count}</div>
-        <button onClick={this.decrement}>decrement</button>
-        <button onClick={this.increment}>increment</button>
+        <div>counter: {this.props.count}</div>
+        <button onClick={this.props.decrement}>decrement</button>
+        <button onClick={this.props.increment}>increment</button>
       </div>
     );
   }
