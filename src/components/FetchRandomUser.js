39972c6d6f58511887f9da21e2c65e2ced2c4fa1
Iqdam Musayyad <iamusayyad@gmail.com>
2021-01-08 15:32:09 +0700

    display arrays

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -3,15 +3,15 @@ import React, { Component } from "react";
 export default class FetchRandomUser extends Component {
   state = {
     loading: true,
-    person: null,
+    people: [],
   };
 
   // fetch is async/await function
   async componentDidMount() {
-    const url = "https://api.randomuser.me/";
+    const url = "https://api.randomuser.me/?results=5";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
+    this.setState({ people: data.results, loading: false });
   }
 
   render() {
@@ -19,19 +19,43 @@ export default class FetchRandomUser extends Component {
       return <div>loading..</div>;
     }
 
-    if (!this.state.person) {
+    if (!this.state.people.length) {
       return <div>didnt get a person</div>;
     }
 
+    // const peopleJsx = this.state.people.map((person, idx) => (
+    //   <div key={person.login.uuid}>
+    //     <div>{person.name.title}</div>
+    //     <div>{person.name.first}</div>
+    //     <div>{person.name.last}</div>
+    //     <img src={person.picture.large} alt={person.picture.large}></img>
+    //   </div>
+    // ));
+
+    // let peopleJsx = [];
+    // this.state.people.forEach((person) => {
+    //   peopleJsx.push(
+    //     <div key={person.login.uuid}>
+    //       <div>{person.name.title}</div>
+    //       <div>{person.name.first}</div>
+    //       <div>{person.name.last}</div>
+    //       <img src={person.picture.large} alt={person.picture.large}></img>
+    //     </div>
+    //   );
+    // });
+
     return (
       <div>
-        <div>{this.state.person.name.title}</div>
-        <div>{this.state.person.name.first}</div>
-        <div>{this.state.person.name.last}</div>
-        <img
-          src={this.state.person.picture.large}
-          alt={this.state.person.picture.large}
-        ></img>
+        {this.state.people.map((person, idx) => (
+          // key must be included and unique string
+          // avoid using index
+          <div key={person.login.uuid}>
+            <div>{person.name.title}</div>
+            <div>{person.name.first}</div>
+            <div>{person.name.last}</div>
+            <img src={person.picture.large} alt={person.picture.large}></img>
+          </div>
+        ))}
       </div>
     );
   }
